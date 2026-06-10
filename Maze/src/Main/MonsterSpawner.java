7f37b0223ac5c0170456b2daed52803d2bf7bca5
Felix Arya <felixarya5@gmail.com>
2026-06-10 11:56:38 +0700

    penting

--- Maze/src/Main/MonsterSpawner.java
+++ Maze/src/Main/MonsterSpawner.java
@@ -47,7 +47,7 @@ public class MonsterSpawner {
                         Math.abs(pixelY - gp.player.y) > gp.getTileSize() * 3) {
 
                         Random rng = new Random();
-                        int rndm = 0;
+                        int rndm = rng.nextInt(3);
                         if(rndm == 0){
                             FireSlime newMonster = new FireSlime(pixelX, pixelY, 1, gp);
                             gp.monsters.add(newMonster);
